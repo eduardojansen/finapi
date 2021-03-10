b5ee626ee4ffdfacf035decba1106511dbb371cf
Eduardo Jansen <eduardo.j@hostdime.com.br>
2021-03-10 16:58:44 -0300

    Criando depósito na conta

--- src/index.js
+++ src/index.js
@@ -64,5 +64,24 @@ app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
   return response.json(customer);
 });
 
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  }
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+
+});
+
+
 
 app.listen(3333)
